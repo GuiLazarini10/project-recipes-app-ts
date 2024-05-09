51ed8392782a6a86904418dc52cd3fdf9b5bbc43
Bruno <brunosgiacomelli@gmail.com>
2024-05-08 21:01:29 -0300

    feat adc reqs 15,16,17 - componente Footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Routes } from 'react-router-dom';
 import Login from './Pages/Login';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={ <Login /> } />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import drinkIcon from '../images/drinkIcon.svg';
+import mealIcon from '../images/mealIcon.svg';
+
+function Footer(): JSX.Element {
+  return (
+    <div
+      data-testid="footer"
+      style={ {
+        width: '100%',
+        position: 'fixed',
+        bottom: 0,
+        display: 'flex',
+        justifyContent: 'space-around',
+        backgroundColor: '#fff',
+        boxShadow: '0px -2px 10px rgba(0,0,0,0.1)',
+        padding: '10px 0',
+      } }
+    >
+      <a href="/drinks">
+        <img data-testid="drinks-bottom-btn" src={ drinkIcon } alt="Drinks" />
+      </a>
+      <a href="/meals">
+        <img data-testid="meals-bottom-btn" src={ mealIcon } alt="Meals" />
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
